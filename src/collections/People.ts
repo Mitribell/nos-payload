7201ff61241ee111a9402772100a62febcebdde4
Dmytro <Mitribell@users.noreply.github.com>
2026-05-16 11:31:40 +0300

    Add People collection

---
+++ src/collections/People.ts
@@ -0,0 +1,28 @@
+import type { CollectionConfig } from 'payload'
+
+export const People: CollectionConfig = {
+  slug: 'people',
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'role',
+      type: 'text',
+    },
+    {
+      name: 'photo',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'bio',
+      type: 'richText',
+    },
+  ],
+}
